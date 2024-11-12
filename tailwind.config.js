bdb18d09d79c808ae000d153a113d272cc27ca44
Yousif <yousefmokhariq@gmail.com>
2024-11-12 14:42:04 +0300

    BETA VERSION

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  daisyui: {
+    themes: [],
+  },
+  plugins: [
+    require('daisyui'),
+  ],
+  
+}
